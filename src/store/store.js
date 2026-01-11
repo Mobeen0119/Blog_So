0da17081b940a49e213e386021296763dc4cdb73
Mobeen0119 <amobeen652@gmail.com>
2026-01-11 21:20:04 +0500

    Change src directory

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, postReducer } from "./slicer.js";
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        post: postReducer,
-    },
-});
